32c1e918a8b047d6ffea3b77a8cf21c440e17cd0
Adriel Miño <adrielsmb@gmail.com>
2023-02-17 17:15:48 -0300

    17/2/2023A

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mentor's Frontend Challenge
+***https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6***
+
+
+**#React - #Tailwind**
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainProducts from "./components/MainProducs/MainProducts";
+import Header from "./components/navbar/Header";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainProducts />
+    </>
+  );
+};
+
+export default App;
